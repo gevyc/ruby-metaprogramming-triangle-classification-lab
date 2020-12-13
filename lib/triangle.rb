733455e9dc941b2538beeb81cf6a8d962675301a
Gevy Cunanan <gevycunanan@icloud.com>
2020-12-13 13:48:09 -0500

    commit tree 0286333ede8196b4369cba0751671be69e4f90be

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  def initialize(a, b, c)
+
+    @a = a 
+    @b = b
+    @c = c
+
+    if a >= (b + c) || b >= (a + c) || c >= (a + b)
+      raise TriangleError
+    end
+
+    if a <= 0 || b <= 0 ||c <= 0
+      raise TriangleError
+    end
+
+    if a == nil || b == nil ||c == nil
+      raise TriangleError
+    end
+
+  end
+
+  def kind
+    if @a == @b && @a == @c
+      return :equilateral
+    elsif @a == @b || @b == @c || @c == @a
+      return :isosceles
+    else
+      return :scalene
+    end
+    
+end
+
+  class TriangleError < StandardError
+  end
+
 end
